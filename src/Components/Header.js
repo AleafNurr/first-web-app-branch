e85e2b4e4a2a9f98b1e6b2d7e6f5340408ced3e9
FishTub <alif.nurrokh@gmail.com>
2022-12-06 08:03:02 +1300

    Test

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(){
     return(
         <header class="border-b font-bold p-3">
-            AppName
+            commit testing
         </header>
     )
 }
